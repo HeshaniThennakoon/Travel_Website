1c344b839c74af7d177d7c5ba7838f0b92ed14ca
Heshani Thennakoon <heshanithennakoon118@gmail.com>
2025-09-30 18:10:33 +0530

    Admin Pangel-settings page create

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -42,5 +42,25 @@
         }
     }
 
+    function update($sql, $values, $datatypes)
+    {
+        $conn = $GLOBALS['conn'];
+        if($stmt = mysqli_prepare($conn, $sql))
+        {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;   
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be excecuted - Update" . mysqli_error($conn));
+            }
+        }
+        else{
+            die("Query cannot be prepared - Update" . mysqli_error($conn));
+        }
+    }
 
 ?>
