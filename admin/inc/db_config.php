02870c27aa3f94a7e8ebb6a09171ef173ce6e667
Heshani Thennakoon <heshanithennakoon118@gmail.com>
2025-10-01 15:21:04 +0530

    Admin panel - Image upload and Delete using AJAX - created

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -21,6 +21,13 @@
         return $data;
     }
 
+    function selectAll($table)
+    {
+        $conn = $GLOBALS['conn'];
+        $res = mysqli_query($conn,"SELECT * FROM $table");
+        return $res;
+    }
+
     function select($sql, $values, $datatypes)
     {
         $conn = $GLOBALS['conn'];
@@ -34,11 +41,11 @@
             }
             else{
                 mysqli_stmt_close($stmt);
-                die("Query cannot be excecuted - Select" . mysqli_error($conn));
+                die("Query cannot be excecuted - Select" );
             }
         }
         else{
-            die("Query cannot be prepared - Select" . mysqli_error($conn));
+            die("Query cannot be prepared - Select" );
         }
     }
 
@@ -55,11 +62,53 @@
             }
             else{
                 mysqli_stmt_close($stmt);
-                die("Query cannot be excecuted - Update" . mysqli_error($conn));
+                die("Query cannot be excecuted - Update");
             }
         }
         else{
-            die("Query cannot be prepared - Update" . mysqli_error($conn));
+            die("Query cannot be prepared - Update");
+        }
+    }
+
+    function insert($sql, $values, $datatypes)
+    {
+        $conn = $GLOBALS['conn'];
+        if($stmt = mysqli_prepare($conn, $sql))
+        {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;   
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be excecuted - Insert" );
+            }
+        }
+        else{
+            die("Query cannot be prepared - Insert");
+        }
+    }
+
+    function delete($sql, $values, $datatypes)
+    {
+        $conn = $GLOBALS['conn'];
+        if($stmt = mysqli_prepare($conn, $sql))
+        {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;   
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be excecuted - Delete" );
+            }
+        }
+        else{
+            die("Query cannot be prepared - Delete" );
         }
     }
 
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,5 +1,15 @@
 <?php
 
+    // frontend (URL used in <img src>)
+    define('SITE_URL','http://127.0.0.1:8080/gh_travelers/');
+    define('ABOUT_IMG_PATH',SITE_URL.'images/about/Team/');
+
+    // backend (filesystem path used for move_uploaded_file)
+    define('UPLOAD_IMAGE_PATH','E:/5.Projects/Travel Website/images/');
+    define('ABOUT_FOLDER','about/Team/');
+
+
+
     function adminLogin()
     {
         session_start();
@@ -31,5 +41,48 @@
             </div>
         alert;        
     }
+
+    function uploadImage($image, $folder)
+    {
+        $valid_mime = ['image/jpeg','image/png','image/webp','image/jpg'];
+        $img_mime = $image['type'];
+
+        if (!in_array($img_mime, $valid_mime)) {
+            return 'inv_img';
+        }
+        else if (($image['size'] / (1024*1024)) > 5) {
+            return 'inv_size';
+        }
+        else {
+            $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+            $rname = 'IMG_' . random_int(11111,99999) . ".$ext";
+
+            // make sure target folder exists
+            $targetDir = UPLOAD_IMAGE_PATH . $folder;
+            if (!is_dir($targetDir)) {
+                mkdir($targetDir, 0755, true);
+            }
+
+            $img_path = $targetDir . $rname;
+
+            if (move_uploaded_file($image['tmp_name'], $img_path)) {
+                return $rname; // return new random filename
+            } else {
+                return 'upd_failed';
+            }
+        }
+    }
+
+
+    function deleteImage($image, $folder)
+    {   
+        $path = UPLOAD_IMAGE_PATH.$folder.$image;
+        if(file_exists($path) && unlink($path)){
+            return true;
+        }
+        return false;
+    }
+
+
 
 ?>
