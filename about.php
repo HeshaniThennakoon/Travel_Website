f9ec97bfdcae17e21e475f9ec8fcf62657589c29
Heshani Thennakoon <heshanithennakoon118@gmail.com>
2025-10-08 01:06:30 +0530

    Frontend - User Registration and Email Verification

--- about.php
+++ about.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GH Travelers - About</title>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
     <?php require('inc/links.php'); ?>
+    <title><?php echo $settings_r['site_title'] ?> - About</title>
+
 
     <style>
         .box{
